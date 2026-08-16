fdcdc9fc6ea226abe663f8f1f03bbe152ae67c4a
Ak ai <abuakasha5143@gmail.com>
2026-08-16 18:28:48 +0500

    Day 2
    AI Journey 13 August

---
+++ F-string.py
@@ -0,0 +1,11 @@
+name = "Abu Akasha"
+Age = 20 
+City = "Lahore"
+
+# old way
+
+print("My name is ", name, ".I'm ", Age,)
+
+# f-string way
+
+print(f"My name is {name}. I'm {Age} years old and I live in {City}. ")
---
+++ Practice.py
@@ -0,0 +1,13 @@
+#Area and Volume of a rectangle
+
+#Area
+length = float(input("Enter the Length of a rectangle: "))
+width = float(input("Enter the Width of a rectangle: "))
+area = length * width
+print(f"Area of a rectangle is {area}cm^2")
+
+#Volume
+height = float(input("Enter the Height of a rectangle: "))
+#Finding the volume of a rectangle 
+Volume = length * width * height
+print(f"Volume of a rectangle is {Volume}cm^3")
