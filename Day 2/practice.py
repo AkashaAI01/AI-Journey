a82129c0ee4861792e070df2258f8bdee553786a
Abu Akasha <abuakasha5143@gmail.com>
2026-08-16 18:03:54 +0500

    Add project files

--- Day 2/practice.py
+++ Day 2/practice.py
@@ -1,4 +1,13 @@
-name = input("What is your name?")
-age = input("Enter your age: ")
-hobby = input("What is your favorite hobby?")
-print(f"Hi! {name}, you are {age} years old. You love {hobby}.")
+#Area and Volume of a rectangle
+
+#Area
+length = float(input("Enter the Length of a rectangle: "))
+width = float(input("Enter the Width of a rectangle: "))
+area = length * width
+print(f"Area of a rectangle is {area}cm^2")
+
+#Volume
+height = float(input("Enter the Height of a rectangle: "))
+#Finding the volume of a rectangle 
+Volume = length * width * height
+print(f"Volume of a rectangle is {Volume}cm^3")
---
+++ Day 4/Accessing lists.py
@@ -0,0 +1,13 @@
+# Creating a list 
+Fruits = ["Banana", "Mango", "Apple", "Orange"]
+
+# Accessing by index
+print(Fruits[0])
+print(Fruits[1])
+print(Fruits[3])
+print(Fruits[-2])
+print(len(Fruits))
+
+Fruits.sort()
+print(Fruits)
+
